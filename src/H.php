e7a6580be8b9fe815ceec9f7b4d4e5e8f0c02743
Ramiro G Glez <mostlyphpsoftware@gmail.com>
2025-11-21 14:20:43 -0700

    modificacion para la validacion semantica html

--- src/H.php
+++ src/H.php
@@ -6,16 +6,20 @@ class H {
     public function __construct (private string $titulo, private string $numero) {}
     public function titulo (string $class = '',string $id = '',string $style = '') : string {
         $numero = $this -> numero;
-        if ($class != '' && $id != '' && $style == '') {
-            return "<h$numero class='".$class."' id='".$id."'>".$this->titulo."</h$numero>";
-        } else if ($class != '' && $id == '' && $style == '') {
-            return "<h$numero class='".$class."'>".$this->titulo."</h$numero>";
-        } else if ($class == '' && $id != '' && $style == '') {
-            return "<h$numero id='".$id."'>".$this->titulo."</h$numero>";
-        } else if ($class == '' && $id == '' && $style != '') {
-            return "<h$numero style='".$style."'>".$this->titulo."</h$numero>";
+        if ($numero !== "1" && $numero >= "2" && $numero <= "5") {
+            if ($class != '' && $id != '' && $style == '') {
+                return "<h$numero class='".$class."' id='".$id."'>".$this->titulo."</h$numero>";
+            } else if ($class != '' && $id == '' && $style == '') {
+                return "<h$numero class='".$class."'>".$this->titulo."</h$numero>";
+            } else if ($class == '' && $id != '' && $style == '') {
+                return "<h$numero id='".$id."'>".$this->titulo."</h$numero>";
+            } else if ($class == '' && $id == '' && $style != '') {
+                return "<h$numero style='".$style."'>".$this->titulo."</h$numero>";
+            } else {
+                return "<h$numero>".$this->titulo."</h$numero>";
+            }
         } else {
-            return "<h$numero>".$this->titulo."</h$numero>";
+            return "titulo no valido.";
         }
     }
 }
