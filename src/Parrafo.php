c68a075e92a5a22a9c779c52ccee8f489abb3c51
Ramiro G Glez <mostlyphpsoftware@gmail.com>
2025-11-21 22:33:21 -0700

    clase Parrafo

---
+++ src/Parrafo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Rmo\Syntaxsanctuary;
+
+class Parrafo {
+    public function __construct (private string $parrafo) {}
+    public function parrafo (string $class = '',string $id = '',string $style = '') : string {
+        if ($class != '' && $id != '' && $style == '') {
+            return "<p class='".$class."' id='".$id."'>".$this->parrafo."</p>";
+        } else if ($class != '' && $id == '' && $style == '') {
+            return "<p class='".$class."'>".$this->parrafo."</p>";
+        } else if ($class == '' && $id != '' && $style == '') {
+            return "<p id='".$id."'>".$this->parrafo."</p>";
+        } else if ($class == '' && $id == '' && $style != '') {
+            return "<p style='".$style."'>".$this->parrafo."</p>";
+        } else {
+            return "<p>".$this->parrafo."</p>";
+        }
+    }
+}
---
+++ tests/ParrafoTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Rmo\Syntaxsanctuary\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Rmo\Syntaxsanctuary\Parrafo;
+
+class ParrafoTest extends TestCase
+{
+    public function testCrearParrafo(): void
+    {
+        // 1. Instanciamos la clase de producción (autoloading en acción)
+        $parrafo = new Parrafo("Bienvenido/a A TFH|SyntaxSanctuary");
+        
+        // 2. Realizamos la prueba
+        $resultado = $parrafo->parrafo();
+
+        // 3. Afirmamos que el resultado es el esperado
+        $this->assertEquals("<p>Bienvenido/a A TFH|SyntaxSanctuary</p>", $resultado);
+
+        //#######################################PRUEBA 2
+
+        // 1. Instanciamos la clase de producción (autoloading en acción)
+        $parrafo = new Parrafo("Bienvenido/a A TFH|SyntaxSanctuary");
+        
+        // 2. Realizamos la prueba
+        $resultado = $parrafo->parrafo(class:"parrafo");
+
+        // 3. Afirmamos que el resultado es el esperado
+        $this->assertEquals("<p class='parrafo'>Bienvenido/a A TFH|SyntaxSanctuary</p>", $resultado);
+
+        //#######################################PRUEBA 3
+        
+        // 1. Instanciamos la clase de producción (autoloading en acción)
+        $parrafo = new Parrafo("Bienvenido/a A TFH|SyntaxSanctuary");
+        
+        // 2. Realizamos la prueba
+        $resultado = $parrafo->parrafo(id:"parrafo");
+
+        // 3. Afirmamos que el resultado es el esperado
+        $this->assertEquals("<p id='parrafo'>Bienvenido/a A TFH|SyntaxSanctuary</p>", $resultado);
+
+        //#######################################PRUEBA 4
+        
+        // 1. Instanciamos la clase de producción (autoloading en acción)
+        $parrafo = new Parrafo("Bienvenido/a A TFH|SyntaxSanctuary");
+        
+        // 2. Realizamos la prueba
+        $resultado = $parrafo->parrafo(style:"color:green;");
+
+        // 3. Afirmamos que el resultado es el esperado
+        $this->assertEquals("<p style='color:green;'>Bienvenido/a A TFH|SyntaxSanctuary</p>", $resultado);
+    }
+}
